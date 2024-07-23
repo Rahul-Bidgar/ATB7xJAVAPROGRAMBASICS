deb273cc5039a91222272d6843060e1863b684b6
Rahul-Bidgar <bidgarr10@gmail.com>
2024-07-23 21:59:30 +0530

    added New files

---
+++ src/EX07072024/Lab006.java
@@ -0,0 +1,15 @@
+package EX07072024;
+
+public class Lab006 {
+    // Can I run class without a main method?
+    // run option?
+//    public static void main(String[] args) { we can write main method also like
+    static public void main(String[] args) {
+
+    }
+    //we can not declare more main method
+    public static void main( ) {
+        System.out.println("Method");
+    }
+    }
+//}
